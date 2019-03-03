896e6b9ddc479768788e30db7ad7b2dce882c1f5
Nbiba Bedis <bedisnbiba@gmail.com>
2019-03-03 20:21:46 +0100

    bette api

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use secret_msg::{decode, encode};
 use std::env::args;
-use std::io::{self, Read};
+use std::fs::File;
+use std::io::{self, Read, Write};
 
 fn main() {
     match args().nth(1).unwrap().to_lowercase().as_str().trim() {
@@ -12,8 +13,13 @@ fn main() {
 
 fn enc() {
     let data = match args().nth(2) {
-        Some(data) => data,
-        None => {
+        Some(data) => {
+            let mut buffer = String::new();
+            let mut file = File::open(data).unwrap();
+            file.read_to_string(&mut buffer).unwrap();
+            buffer
+        }
+        None if args().len() == 2 => {
             let mut buffer = String::new();
             let stdin = io::stdin();
             let mut handle = stdin.lock();
@@ -21,26 +27,40 @@ fn enc() {
             handle.read_to_string(&mut buffer).unwrap();
             buffer
         }
+        None => {
+            let mut buffer = String::new();
+            io::stdin().read_line(&mut buffer).unwrap();
+            buffer
+        }
     };
 
     let enc_data = encode(&data);
-    enc_data.0.lines().for_each(|l| {
-        println!("{}", l);
-    });
-    println!("--------------------\nkey:{}", &enc_data.1);
+    let mut enc_file = File::create("./enc").unwrap();
+    write!(enc_file, "{}", enc_data.0).unwrap();
+    println!("Key: {}", enc_data.1);
 }
 
 fn dec() {
     let key = args().nth(2).unwrap().parse::<usize>().unwrap();
 
     let data = match args().nth(3) {
-        Some(data) => data,
-        None => {
+        Some(data) => {
+            let mut buffer = String::new();
+            let mut file = File::open(data).unwrap();
+            file.read_to_string(&mut buffer).unwrap();
+            buffer
+        }
+        None if args().len() == 2 => {
             let mut buffer = String::new();
             let stdin = io::stdin();
             let mut handle = stdin.lock();
 
             handle.read_to_string(&mut buffer).unwrap();
+            buffer
+        }
+        None => {
+            let mut buffer = String::new();
+            io::stdin().read_line(&mut buffer).unwrap();
             buffer
         }
     };
